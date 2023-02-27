1d4aeb8640dd1fb9fbcee380938ebdf380474cd1
RajBaviskar <rajeshbaviskar.1987@gmail.com>
2023-02-26 23:08:35 -0800

    Update serviceCreate.tf
    adding provider

--- serviceCreate.tf
+++ serviceCreate.tf
@@ -29,3 +29,9 @@ resource "harness_platform_service" "this" {
         type: Kubernetes
        EOT
 }
+
+provider "harness" {
+  endpoint         = "https://app.harness.io/gateway"
+  account_id       = "UKh5Yts7THSMAbccG3HrLA"
+  platform_api_key = "pat.UKh5Yts7THSMAbccG3HrLA.631ad184015b135d5052742a.VsueShc2p2xD3abapFQG"
+}
