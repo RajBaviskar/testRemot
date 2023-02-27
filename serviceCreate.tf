13e095a0b4945b6b2ef491f3ea3b78b9a0c7da72
RajBaviskar <rajeshbaviskar.1987@gmail.com>
2023-02-26 23:39:15 -0800

    dsd

--- serviceCreate.tf
+++ serviceCreate.tf
@@ -21,21 +21,39 @@ resource "harness_platform_service" "this" {
       tags: {}
       serviceDefinition:
         spec:
-          artifacts:
-            primary:
-              primaryArtifactRef: <+input>
-              sources:
-                - name: test
-                  identifier: test
-                  template:
-                    templateRef: account.TestCDS53213
-                    versionLabel: "1"
-                    templateInputs:
-                      type: DockerRegistry
-                      spec:
-                        imagePath: <+input>
-                        tag: <+input>
+          manifests:
+            - manifest:
+                identifier: manifest1
+                type: K8sManifest
+                spec:
+                  store:
+                    type: Github
+                    spec:
+                      connectorRef: <+input>
+                      gitFetchType: Branch
+                      paths:
+                        - files1
+                      repoName: <+input>
+                      branch: master
+                  skipResourceVersioning: false
+          configFiles:
+            - configFile:
+                identifier: configFile1
+                spec:
+                  store:
+                    type: Harness
+                    spec:
+                      files:
+                        - <+org.description>
+          variables:
+            - name: var1
+              type: String
+              value: val1
+            - name: var2
+              type: String
+              value: val2
         type: Kubernetes
+      gitOpsEnabled: false
        EOT
 }
 
