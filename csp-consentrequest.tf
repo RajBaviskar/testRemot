8197c9abdbe418a1896fd30e62d56ab50c933cb2
RajBaviskar <rajeshbaviskar.1987@gmail.com>
2023-03-15 12:41:06 -0700

    Update csp-consentrequest.tf

--- csp-consentrequest.tf
+++ csp-consentrequest.tf
@@ -56,7 +56,9 @@ resource "harness_platform_service" "ng" {
                   gitOpsEnabled: false
               EOT
 }
+
 provider "harness" {
-  endpoint         = "https://harness-ng.paloaltonetworks.com"
-  #endpoint         ="https://app.harness.io/gateway"
+  endpoint         = "https://app.harness.io/gateway"
+  account_id       = "UKh5Yts7THSMAbccG3HrLA"
+  platform_api_key = "pat.UKh5Yts7THSMAbccG3HrLA.631ad184015b135d5052742a.VsueShc2p2xD3abapFQG"
 }
