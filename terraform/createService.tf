6024fd0b6cb40d263b5fd2167b5ee981c90cb57d
RajBaviskar <rajendra.baviskar@harness.io>
2024-07-15 14:32:00 -0700

    Create createService.tf

---
+++ terraform/createService.tf
@@ -0,0 +1,44 @@
+resource "harness_platform_service" "this" {
+  identifier   = "rajK8NativeTF2"
+  name         = "rajK8NativeTF2"
+  org_id       = "default"
+  project_id   = "rajTest"
+  force_delete = "true"
+  yaml         = <<-EOH
+service:
+  name: rajK8NativeTF2
+  identifier: rajK8NativeTF2
+  orgIdentifier: default
+  projectIdentifier: rajTest
+  serviceDefinition:
+    spec:
+      manifests:
+        - manifest:
+            identifier: nativek8
+            type: K8sManifest
+            spec:
+              store:
+                type: Github
+                spec:
+                  connectorRef: account.rajAccountGithub
+                  gitFetchType: Branch
+                  paths:
+                    - kubernetes/native
+                  repoName: testRemot
+                  branch: main
+              skipResourceVersioning: false
+              enableDeclarativeRollback: false
+      artifacts:
+        primary:
+          primaryArtifactRef: <+input>
+          sources:
+            - spec:
+                connectorRef: rajharnessdev
+                imagePath: harnessdev/rajtodoapp
+                tag: "4"
+                digest: ""
+              identifier: todofrompipeline
+              type: DockerRegistry
+    type: Kubernetes
+  EOH
+}
