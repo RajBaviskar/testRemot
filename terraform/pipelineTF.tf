3de3f9d6d22173c831eb25a8c453c6ba136a8c2c
RajBaviskar <rajendra.baviskar@harness.io>
2024-08-03 00:17:47 -0700

    Update pipelineTF.tf

--- terraform/pipelineTF.tf
+++ terraform/pipelineTF.tf
@@ -5,115 +5,115 @@ resource "harness_platform_pipeline" "example" {
   name       = "VeryBigPipelineTest"
   yaml = <<-EOT
   pipeline:
-  name: VeryBigPipe
-  identifier: VeryBigPipe
-  projectIdentifier: rajTest
-  orgIdentifier: default
-  tags: {}
-  stages:
-    - stage:
-        name: stage1
-        identifier: stage1
-        tags: {}
-        template:
-          templateRef: asfsdfd
-          versionLabel: "1"
-          templateInputs:
-            type: Deployment
-            spec:
-              environment:
-                environmentRef: <+input>
-                environmentInputs: <+input>
-                serviceOverrideInputs: <+input>
-                infrastructureDefinitions: <+input>
-              service:
-                serviceRef: <+input>
-                serviceInputs: <+input>
-    - stage:
-        name: stage2
-        identifier: stage2
-        tags: {}
-        template:
-          templateRef: fdsg
-          versionLabel: "1"
-          templateInputs:
-            type: Deployment
-            spec:
-              service:
-                serviceInputs:
-                  serviceDefinition:
-                    type: Kubernetes
-                    spec:
-                      artifacts:
-                        primary:
-                          primaryArtifactRef: <+input>
-                          sources: <+input>
-    - stage:
-        name: stage3
-        identifier: stage3
-        tags: {}
-        template:
-          templateRef: helmDeploy
-          versionLabel: v1
-          templateInputs:
-            type: Deployment
-            spec:
-              service:
-                serviceRef: <+input>
-                serviceInputs: <+input>
-              environment:
-                environmentRef: <+input>
-                infrastructureDefinitions: <+input>
-                environmentInputs: <+input>
-                serviceOverrideInputs: <+input>
-              execution:
-                steps:
-                  - stepGroup:
-                      identifier: deployContainers
-                      steps:
-                        - step:
-                            identifier: chartDeploymentBarrier
-                            type: Barrier
-                            spec:
-                              barrierRef: <+input>
-                  - stepGroup:
-                      identifier: verifyService
-                      steps:
-                        - step:
-                            identifier: chartTestingComplete
-                            type: Barrier
-                            spec:
-                              barrierRef: <+input>
-            variables:
-              - name: Chart_Version
-                type: String
-                value: <+input>
-            when:
-              condition: <+input>
-    - stage:
-        name: stage4
-        identifier: stage4
-        tags: {}
-        template:
-          templateRef: sdgfgghgh
-          versionLabel: "1"
-          templateInputs:
-            type: Deployment
-            spec:
-              service:
-                serviceRef: <+input>
-                serviceInputs: <+input>
-              environment:
-                environmentRef: <+input>
-                environmentInputs: <+input>
-                serviceOverrideInputs: <+input>
-                infrastructureDefinitions: <+input>
-    - stage:
-        name: stage5
-        identifier: stage5
-        tags: {}
-        template:
-          templateRef: CDS74562remoteTemp
-          versionLabel: "2"
+    name: VeryBigPipelineTest
+    identifier: VeryBigPipelineTest
+    projectIdentifier: rajTest
+    orgIdentifier: default
+    tags: {}
+    stages:
+      - stage:
+          name: stage1
+          identifier: stage1
+          tags: {}
+          template:
+            templateRef: asfsdfd
+            versionLabel: "1"
+            templateInputs:
+              type: Deployment
+              spec:
+                environment:
+                  environmentRef: <+input>
+                  environmentInputs: <+input>
+                  serviceOverrideInputs: <+input>
+                  infrastructureDefinitions: <+input>
+                service:
+                  serviceRef: <+input>
+                  serviceInputs: <+input>
+      - stage:
+          name: stage2
+          identifier: stage2
+          tags: {}
+          template:
+            templateRef: fdsg
+            versionLabel: "1"
+            templateInputs:
+              type: Deployment
+              spec:
+                service:
+                  serviceInputs:
+                    serviceDefinition:
+                      type: Kubernetes
+                      spec:
+                        artifacts:
+                          primary:
+                            primaryArtifactRef: <+input>
+                            sources: <+input>
+      - stage:
+          name: stage3
+          identifier: stage3
+          tags: {}
+          template:
+            templateRef: helmDeploy
+            versionLabel: v1
+            templateInputs:
+              type: Deployment
+              spec:
+                service:
+                  serviceRef: <+input>
+                  serviceInputs: <+input>
+                environment:
+                  environmentRef: <+input>
+                  infrastructureDefinitions: <+input>
+                  environmentInputs: <+input>
+                  serviceOverrideInputs: <+input>
+                execution:
+                  steps:
+                    - stepGroup:
+                        identifier: deployContainers
+                        steps:
+                          - step:
+                              identifier: chartDeploymentBarrier
+                              type: Barrier
+                              spec:
+                                barrierRef: <+input>
+                    - stepGroup:
+                        identifier: verifyService
+                        steps:
+                          - step:
+                              identifier: chartTestingComplete
+                              type: Barrier
+                              spec:
+                                barrierRef: <+input>
+              variables:
+                - name: Chart_Version
+                  type: String
+                  value: <+input>
+              when:
+                condition: <+input>
+      - stage:
+          name: stage4
+          identifier: stage4
+          tags: {}
+          template:
+            templateRef: sdgfgghgh
+            versionLabel: "1"
+            templateInputs:
+              type: Deployment
+              spec:
+                service:
+                  serviceRef: <+input>
+                  serviceInputs: <+input>
+                environment:
+                  environmentRef: <+input>
+                  environmentInputs: <+input>
+                  serviceOverrideInputs: <+input>
+                  infrastructureDefinitions: <+input>
+      - stage:
+          name: stage5
+          identifier: stage5
+          tags: {}
+          template:
+            templateRef: CDS74562remoteTemp
+            versionLabel: "2"
   EOT
 }
