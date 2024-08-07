c89ebc27e0ef550be452d347d61aedd719d62b5a
RajBaviskar <rajendra.baviskar@harness.io>
2024-08-07 01:10:27 -0700

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -6,3 +6,10 @@ terraform {
   }
   # required_version = "1.3.9"
 }
+
+provider "harness" {
+   endpoint         = "https://smp2.harnesscx.io/gateway"
+   account_id       = "pLaOZ3e-QmWOJld7ANsqow"
+   platform_api_key = "pat.pLaOZ3e-QmWOJld7ANsqow.66b27f1203e5696ad707eaeb.5wva1kytOif7oiY4NgG5"
+}
+
