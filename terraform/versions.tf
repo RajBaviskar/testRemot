2813767861c78ea18062be997b967ede7e036c27
RajBaviskar <rajendra.baviskar@harness.io>
2024-08-14 00:09:51 -0700

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -10,6 +10,6 @@ terraform {
 provider "harness" {
   endpoint         = "https://app.harness.io/gateway"
   account_id       = "UKh5Yts7THSMAbccG3HrLA"
-  platform_api_key = "pat.UKh5Yts7THSMAbccG3HrLA.6530352e1bc3b44b9f0cd0c4.M7iEe7prb252Os0u6kSw"
+  platform_api_key = ""
 }
 
