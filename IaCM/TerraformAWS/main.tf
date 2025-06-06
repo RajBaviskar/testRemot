0e58567647a5450046107f4642bddccfa4b73aec
RajBaviskar <rajendra.baviskar@harness.io>
2025-06-05 22:50:00 -0700

    Update main.tf

--- IaCM/TerraformAWS/main.tf
+++ IaCM/TerraformAWS/main.tf
@@ -6,3 +6,21 @@ resource "aws_instance" "example" {
     Name = var.instance_name
   }
 }
+
+# VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "example-vpc"
+  }
+}
+
+# S3 Bucket
+resource "aws_s3_bucket" "example" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name = "example-bucket"
+  }
+}
