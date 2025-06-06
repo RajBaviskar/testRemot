7281a3e53f35833f1f345450a05644d110f4264b
RajBaviskar <rajendra.baviskar@harness.io>
2025-06-05 22:48:56 -0700

    Update variables.tf

--- IaCM/TerraformAWS/variables.tf
+++ IaCM/TerraformAWS/variables.tf
@@ -15,3 +15,9 @@ variable "instance_name" {
   type        = string
   default = "raj-tf-ec2"
 }
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+  default     = "raj-tf-s3-bucket"
+}
