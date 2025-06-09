2d3ff54a5906126103cbab3f6c6a865dfc66d245
RajBaviskar <rajendra.baviskar@harness.io>
2025-06-09 14:12:26 +0100

    Update main.tf

--- IaCM/SimpleTF/main.tf
+++ IaCM/SimpleTF/main.tf
@@ -8,6 +8,15 @@ terraform {
   }
 }
 
+backend "s3" {
+    bucket = "rajtfbasic-local1"  # Your S3 bucket
+    key    = "SimpleTF/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+    # Optional: Add DynamoDB table for state locking
+    dynamodb_table = "terraform-locks"
+}
+
 resource "local_file" "example" {
   content  = "Hello from Terraform!"
   filename = "${path.module}/example.txt"
