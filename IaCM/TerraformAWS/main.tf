a51b3599712c49005e5cf730261be960ef0b6649
Rajendra Baviskar <rajendra.baviskar@harness.io>
2025-11-17 12:27:51 -0800

    remove VPC

--- IaCM/TerraformAWS/main.tf
+++ IaCM/TerraformAWS/main.tf
@@ -7,15 +7,6 @@ resource "aws_instance" "example" {
   }
 }
 
-# VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "example-vpc"
-  }
-}
-
 # S3 Bucket
 resource "aws_s3_bucket" "example" {
   bucket = var.bucket_name
