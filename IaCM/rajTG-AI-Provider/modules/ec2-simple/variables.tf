9c7c3f0f64e73e0a2f28187dabf6af4ba74cf995
Rajendra Baviskar <rajendra.baviskar@harness.io>
2026-02-26 15:32:54 -0800

    changed

--- IaCM/rajTG-AI-Provider/modules/ec2-simple/variables.tf
+++ IaCM/rajTG-AI-Provider/modules/ec2-simple/variables.tf
@@ -7,7 +7,7 @@ variable "instance_count" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
+  default     = "t2.small"
 }
 
 variable "environment" {
