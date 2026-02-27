8cc068b212ab4cc5cefad306ef59cd9b5f36eacf
RajBaviskar <rajendra.baviskar@harness.io>
2026-02-26 17:00:51 -0800

    Update IaCM/rajTG-AI-Provider/modules/ec2-simple/main.tf

--- IaCM/rajTG-AI-Provider/modules/ec2-simple/main.tf
+++ IaCM/rajTG-AI-Provider/modules/ec2-simple/main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "this" {
   # This worked in provider 4.x, but provider 5.x changed validation rules
   # Cannot specify both subnet_id and vpc_security_group_ids with network_interface attachment
   subnet_id              = "subnet-12345"
-  vpc_security_group_ids = ["sg-12345"]
+# vpc_security_group_ids = ["sg-12345"]
   
   network_interface {
     device_index          = 0
