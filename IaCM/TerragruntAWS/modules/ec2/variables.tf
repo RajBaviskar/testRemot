090d82fc24694ec571ac3c2c76c6fc5f63355573
RajBaviskar <rajendra.baviskar@harness.io>
2026-04-02 14:11:41 -0700

    Delete IaCM/TerragruntAWS directory

--- IaCM/TerragruntAWS/modules/ec2/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "ami_id" {}
-variable "instance_type" {}
-variable "instance_name" {}
-variable "aws_region" {
-  description = "The AWS region to deploy resources in"
-  type        = string
-}
