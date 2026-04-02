16e144551283350bd6997afeb259b073292f74cf
RajBaviskar <rajendra.baviskar@harness.io>
2026-04-02 13:28:08 -0700

    Delete IaCM/TGMultiAccount directory

--- IaCM/TGMultiAccount/modules/ec2/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "aws_region" {
-  description = "The AWS region to deploy resources in"
-  type        = string
-}
-
-variable "ami" {}
-variable "instance_type" {}
-variable "instance_name" {}
